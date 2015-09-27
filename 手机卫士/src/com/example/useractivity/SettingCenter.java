42858ffd28820d87c40610e0a1002181fea59fbb
ITtoken <527623439@qq.com>
2015-09-27 23:09:09 +0800

    12|优化本地数据库查询代码，提高复用性+来电显示归属地功能

--- 手机卫士/src/com/example/useractivity/SettingCenter.java
+++ 手机卫士/src/com/example/useractivity/SettingCenter.java
@@ -1,6 +1,7 @@
 package com.example.useractivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -9,8 +10,8 @@ import android.widget.CheckBox;
 import android.widget.TextView;
 
 import com.example.mobilesafe.R;
+import com.example.service.CommingShowService;
 import com.example.splash.SettingRelativeLayout;
-import com.example.utils.AppConst;
 import com.example.utils.FileInstance;
 
 public class SettingCenter extends Activity implements OnClickListener {
@@ -21,6 +22,9 @@ public class SettingCenter extends Activity implements OnClickListener {
 	private SettingRelativeLayout location_show;
 	private CheckBox cb_location;
 	private TextView desc_location;
+	private SettingRelativeLayout comming_show;
+	private CheckBox cb_comming_show;
+	private TextView desc_comming_show;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +39,7 @@ public class SettingCenter extends Activity implements OnClickListener {
 		sr.setDefaultStat(stat);
 		sr.setOnClickListener(this);
 
-		/* 归属查询 设置*/
+		/* 归属查询 设置 */
 		boolean location_stat = pref.getBoolean("location_stat", false);
 		location_show = (SettingRelativeLayout) findViewById(R.id.location_show);
 		cb_location = (CheckBox) location_show.findViewById(R.id.cb);
@@ -43,6 +47,13 @@ public class SettingCenter extends Activity implements OnClickListener {
 		location_show.setDefaultStat(location_stat);
 		location_show.setOnClickListener(this);
 
+		/* 来电显示归属地 */
+		boolean commingshow_stat = pref.getBoolean("commingshow_stat", false);
+		comming_show = (SettingRelativeLayout) findViewById(R.id.comming_show);
+		cb_comming_show = (CheckBox) comming_show.findViewById(R.id.cb);
+		desc_comming_show = (TextView) comming_show.findViewById(R.id.desc);
+		comming_show.setDefaultStat(commingshow_stat);
+		comming_show.setOnClickListener(this);
 	}
 
 	@Override
@@ -51,23 +62,36 @@ public class SettingCenter extends Activity implements OnClickListener {
 		case R.id.sr:
 			if (cb.isChecked()) {
 				cb.setChecked(false);
-				desc.setText("自动更新已关闭");
+				desc.setText("关闭");
 				pref.edit().putBoolean("stat", false).commit();
 			} else {
 				cb.setChecked(true);
-				desc.setText("自动更新已开启");
+				desc.setText("开启");
 				pref.edit().putBoolean("stat", true).commit();
 			}
 			break;
 		case R.id.location_show:
 			if (cb_location.isChecked()) {
 				cb_location.setChecked(false);
-				desc_location.setText("自动捕捉已关闭");
+				desc_location.setText("关闭");
 				pref.edit().putBoolean("location_stat", false).commit();
 			} else {
 				cb_location.setChecked(true);
-				desc_location.setText("自动捕捉已开启");
+				desc_location.setText("开启");
 				pref.edit().putBoolean("location_stat", true).commit();
+			}
+			break;
+		case R.id.comming_show:
+			if (cb_comming_show.isChecked()) {
+				cb_comming_show.setChecked(false);
+				desc_comming_show.setText("关闭");
+				pref.edit().putBoolean("commingshow_stat", false).commit();
+				stopService(new Intent(this, CommingShowService.class));
+			} else {
+				cb_comming_show.setChecked(true);
+				desc_comming_show.setText("开启");
+				pref.edit().putBoolean("commingshow_stat", true).commit();
+				startService(new Intent(this, CommingShowService.class));
 			}
 			break;
 
